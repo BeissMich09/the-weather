67bcab2da4ba987002b863502a2ef652bf0bc2d1
BeissenMich <blackyellovv@gmail.com>
2021-02-05 16:21:50 +0300

    css styles 1.1

--- src/Components/CurrentWeather/CurrentWeather.jsx
+++ src/Components/CurrentWeather/CurrentWeather.jsx
@@ -10,22 +10,31 @@ const CurrentWeather = (props) => {
   ];
   return (
     <div className={style.item}>
+      <h2>
+        {props.weather.data[0].city_name}{" "}
+        <i class="fas fa-city"></i>
+      </h2>
       <div className={style.genegalParams}>
         <p>
-          {props.weather.data[0].city_name}: {props.weather.data[0].temp} °C, по
-          ощущениям: {props.weather.data[0].app_temp} °C
+          <i class="fas fa-temperature-low"></i> {props.weather.data[0].temp}{" "}
+          °C, по ощущениям: {props.weather.data[0].app_temp} °C
         </p>
         {/* <p>{icon}</p> */}
       </div>
-
       <p>Описание: {description}</p>
       <p>
-        Скорость ветра: {props.weather.data[0].wind_spd} м/с{" "}
+        Скорость ветра <i class="fas fa-wind"></i>:{" "}
+        {props.weather.data[0].wind_spd} м/с <i class="fas fa-compass"></i>
         {props.weather.data[0].wind_cdir}
       </p>
 
-      <p>Влажность: {props.weather.data[0].rh}%</p>
-      <p>Покрытие облаками:{props.weather.data[0].clouds}% </p>
+      <p>
+        Влажность <i class="fas fa-tint"></i>: {props.weather.data[0].rh}%
+      </p>
+      <p>
+        Покрытие облаками <i class="fas fa-cloud"></i>:
+        {props.weather.data[0].clouds}%{" "}
+      </p>
     </div>
   );
 };
